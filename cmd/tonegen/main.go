77ad6fa83a39c9da02f8e6dc0fc7badce7daf076
Tom <twitchyliquid64@ciphersink.net>
2018-10-10 16:06:04 -0700

    Add instructions to tonegen.go

--- cmd/tonegen/main.go
+++ cmd/tonegen/main.go
@@ -9,10 +9,16 @@ import (
 	liquiddsp "github.com/twitchyliquid64/go-liquiddsp"
 )
 
-func main() {
-	buff := liquiddsp.MakeTone(8000, 8000, 440)
+// go run cmd/tonegen/main.go | aplay -f S16_LE -r 44100
 
-	amp := float32(math.Pow(2, 15)) * 0.7
+const (
+	sampleRate = 44100
+)
+
+func main() {
+	buff := liquiddsp.MakeTone(sampleRate, sampleRate, 440)
+
+	amp := float32(math.Pow(2, 15)) * 0.8
 
 	for i := range buff {
 		sample := int16(real(buff[i]) * amp)
